66116c34d5fbdaba61ace9bf16033ad85489734a
Brandon Frye <brandonkfrye@gmail.com>
2015-05-09 18:19:03 -0400

    modifications

--- header.php
+++ header.php
@@ -45,9 +45,6 @@
 	<body <?php body_class(); ?>>
 	<?php do_action( 'foundationpress_after_body' ); ?>
 	
-	<div class="off-canvas-wrap" data-offcanvas>
-	<div class="inner-wrap">
-	
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 	
 	<nav class="tab-bar show-for-small-only">
@@ -60,8 +57,6 @@
 
 		</section>
 	</nav>
-
-	<?php get_template_part( 'parts/off-canvas-menu' ); ?>
 
 	<?php get_template_part( 'parts/top-bar' ); ?>
 
