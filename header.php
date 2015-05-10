4b43aded6bfa27dcff1a2b49d8809eb9f4df627e
Brandon Frye <brandonkfrye@gmail.com>
2015-05-10 19:05:58 -0400

    add color, work with links, and add logo to header

--- header.php
+++ header.php
@@ -23,12 +23,17 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nctsa' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
+
+
+	<header class="header" role="banner">
 		<nav class="main-navigation" role="navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+
+
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php bloginfo( 'template_directory' ); ?>/img/logo.png"></a>
+			<!-- <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
 			<!-- <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2> -->
 		</div><!-- .site-branding -->
 
