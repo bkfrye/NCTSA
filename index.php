9d254b154f6927f81c13d18445926d5e3c133327
Brandon Frye <brandonkfrye@gmail.com>
2015-05-12 00:36:46 -0400

    reformatting index

--- index.php
+++ index.php
@@ -22,37 +22,35 @@ get_header(); ?>
 
 
 <!-- wordpress loop -->
-				<div class="row">
-					<div class="small-12 medium-8 announcements columns">
-						<?php if ( have_posts() ) : ?>
+				<section>
+					<div class="row">
+						<div class="small-12 medium-8 announcements columns">
+							<?php if ( have_posts() ) : ?>
 
-							<?php /* Start the Loop */ ?>
-							<?php while ( have_posts() ) : the_post(); ?>
+								<?php /* Start the Loop */ ?>
+								<?php while ( have_posts() ) : the_post(); ?>
 
-								<?php
-									get_template_part( 'template-parts/content', get_post_format() );
-								?>
+									<?php
+										get_template_part( 'template-parts/content', get_post_format() );
+									?>
 
-							<?php endwhile; ?>
+								<?php endwhile; ?>
 
-							<?php the_posts_navigation(); ?>
+								<?php the_posts_navigation(); ?>
 
-						<?php else : ?>
+							<?php else : ?>
 
-							<?php get_template_part( 'template-parts/content', 'none' ); ?>
+								<?php get_template_part( 'template-parts/content', 'none' ); ?>
 
-						<?php endif; ?>
+							<?php endif; ?>
 
+						</div>
+
+						<div class="small-10 medium-4 columns">
+							<?php get_sidebar(); ?>
+						</div>
 					</div>
-
-					<div class="small-10 medium-4 columns">
-						<?php get_sidebar(); ?>
-					</div>
-
-
-
-
-				</div>
+				</section>
 <!-- wordpress loop ends -->
 
 						
