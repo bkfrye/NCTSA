dc706695e7ec0edb05f5bc303611db670cfdf907
Brandon Frye <brandonkfrye@gmail.com>
2015-05-14 13:45:53 -0400

    updating homepage template

--- homepage.php
+++ homepage.php
@@ -1,15 +1,10 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Template Name: Homepage
  *
  * @package nctsa
  */
+
 get_header(); ?>
 
 
@@ -24,17 +19,11 @@ get_header(); ?>
 				<section>
 					<div class="row">
 						<div class="small-12 medium-8 announcements columns">
-							<?php if ( have_posts() ) : ?>
-								<?php /* Start the Loop */ ?>
-								<?php while ( have_posts() ) : the_post(); ?>
-									<?php
-										get_template_part( 'template-parts/content', get_post_format() );
-									?>
-								<?php endwhile; ?>
-								<?php the_posts_navigation(); ?>
-							<?php else : ?>
-								<?php get_template_part( 'template-parts/content', 'none' ); ?>
-							<?php endif; ?>
+
+
+								<!-- DISPLAY LATEST POST HERE -->
+
+
 						</div>
 
 						<div class="small-10 medium-4 columns">
@@ -95,5 +84,3 @@ get_header(); ?>
 	</div>
 </div>
 <?php get_footer(); ?>
-
-
